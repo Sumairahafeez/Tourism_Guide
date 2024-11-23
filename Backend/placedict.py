3a66aed4439a2d2244100f9d1a57faf9307e8dbe
Sumaira Hafeez <144407143+Sumairahafeez@users.noreply.github.com>
2024-11-23 19:16:19 +0500

    places new functions

--- Backend/placedict.py
+++ Backend/placedict.py
@@ -9,13 +9,14 @@ def ReadCsv():
             for row in reader:
                 print(row)
                 places.append({
-                    'name': row[0],
-                    'ratings': row[1],
-                    'category': row[2]
+                    'Name': row[0],
+                    'Ratings': row[1],
+                    'Category': row[2]
                 })
-                print("places loaded successfully")    
+                print("places loaded successfully")        
     except FileNotFoundError:
         print('File not found')  
+    return places    
 def writeCsv(places):
     try:
         with open(file=csv_file, mode = 'w') as file:
@@ -27,18 +28,26 @@ def writeCsv(places):
         print('File not found')                         
 def AddPlace(name,ratings,category):
     places = ReadCsv()
-    if any(places['name'].lower() == name.lower() for place in places):
+    if any(places['Name'].lower() == name.lower() for place in places):
         print('Place already exists')
     else:
         places.append({
-            'name': name,
-            'ratings': ratings,
-            'category': category
+            'Name': name,
+            'Ratings': ratings,
+            'Category': category
         })
         writeCsv(places)
 def GetTop6Places():
     places = ReadCsv()
-    Ratings = [places['ratings'] for rating in places]  
+    Ratings = [place['Ratings'] for place in places]  
     sortingAlgorithms.bubbleSort(Ratings)
     top6 = Ratings[:6]
-    return [place for place in places if place['ratings'] in top6]
+    return [place for place in places if place['Ratings'] in top6]
+def GetRandomPlaces():
+    places = ReadCsv()
+    RandomPlaces = [place['Ratings'] for place in places]
+    sortingAlgorithms.shuffleArray(RandomPlaces)
+    return [place for place in places if place['Ratings'] in RandomPlaces[:6]]
+def GetPlacesByCategory(category):
+    places = ReadCsv()
+    return [place for place in places if place['Category'].lower == category.lower()]  
--- Backend/sortingAlgorithms.py
+++ Backend/sortingAlgorithms.py
@@ -1,10 +1,13 @@
-class algorithms:
-    def __init__(self):
-        pass
-    def bubbleSort(self, arr):
+import random
+def bubbleSort(arr):
         n = len(arr)
         for i in range(n):
-            for j in range(0, n-j-1):
+            for j in range(0, n-i-1):
                 if arr[j] > arr[j+1]:
                     arr[j], arr[j+1] = arr[j+1] , arr[j]
+def shuffleArray(arr):
+    n = len(arr)
+    for i in range(n):
+        j = random.randint(0,n-1)
+        arr[i], arr[j] = arr[j], arr[i]                    
                     
